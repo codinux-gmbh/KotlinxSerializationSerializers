7d9e155734fb25d7a2a1913d424838585e29a705
dankito <git@dankito.net>
2025-01-23 11:42:04 +0100

    Converted to Kotlin multiplatform project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,101 @@
+@file:Suppress("UnstableApiUsage")
+
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+
+
 plugins {
-    kotlin("jvm") version "2.1.0"
+    kotlin("multiplatform")
+    kotlin("plugin.serialization")
 }
 
-group = "net.codinux.xmp"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
 
-tasks.test {
-    useJUnitPlatform()
-}
+group = "net.codinux.kotlin.serialization"
+version = "1.0.0-SNAPSHOT"
+
+
 kotlin {
     jvmToolchain(11)
+
+    jvm()
+
+
+    js {
+        moduleName = "kotlinx-serialization-serializers"
+        binaries.executable()
+
+        browser {
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                    useFirefoxHeadless()
+                }
+            }
+        }
+
+        nodejs {
+            testTask {
+                useMocha {
+                    timeout = "20s" // Mocha times out after 2 s, which is too short for some tests
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser {
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                    useFirefoxHeadless()
+                }
+            }
+        }
+    }
+
+
+    linuxX64()
+    mingwX64()
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    macosX64()
+    macosArm64()
+    watchosArm64()
+    watchosSimulatorArm64()
+    tvosArm64()
+    tvosSimulatorArm64()
+
+    applyDefaultHierarchyTemplate()
+
+
+    val kotlinxSerializationVersion: String by project
+
+    val assertKVersion: String by project
+    val jacksonVersion: String by project
+    val logbackVersion: String by project
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$kotlinxSerializationVersion")
+        }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+
+            implementation("com.willowtreeapps.assertk:assertk:$assertKVersion")
+        }
+
+        jvmTest.dependencies {
+            implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+            implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+
+            implementation("ch.qos.logback:logback-classic:$logbackVersion")
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,21 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+pluginManagement {
+    val kotlinVersion: String by settings
+
+
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        google()
+    }
+
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        kotlin("multiplatform") version kotlinVersion
+        kotlin("plugin.serialization") version kotlinVersion
+    }
 }
 
 
-rootProject.name = "kotlinx-serialization-serializers"
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
